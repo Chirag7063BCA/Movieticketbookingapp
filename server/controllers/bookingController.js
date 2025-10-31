3cd369b139b3490597ab80bc03ea7485990fc998
Chirag7063BCA <160454401+Chirag7063BCA@users.noreply.github.com>
2025-10-31 21:10:46 +0530

    function added to cancel after 10 min

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,3 +1,4 @@
+import { inngest } from "../inngest/index.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
 import stripe from "stripe";
@@ -74,6 +75,14 @@ export const createBooking = async (req, res) => {
 
     booking.paymentLink = session.url;
     await booking.save();
+
+     // Run Inngest Shedular function to check payment status after 10 min
+    await inngest.send({
+      name: "app/checkpayment",
+      data: {
+        bookingId: booking._id.toString(),
+      },
+    });
 
      res.json({success: true, url: session.url})
     } catch(error) {
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Booking from "../models/Booking.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -48,8 +49,104 @@ const syncUserUpdation = inngest.createFunction(
   }
 )
 
+// Inngest function to cancel booking and release seats of show after 10 min of booking created if the payment is not done.
+const releaseSeatsAndDeleteBooking = inngest.createFunction(
+  { id: "release-seats-delete-booking" },
+  { event: "app/checkpayment" },
+  async ({ event, step }) => {
+    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+    await step.sleepUntil("wait-for-10-minutes", tenMinutesLater);
+
+    await step.run("check-payment-status", async () => {
+      const bookingId = event.data.bookingId;
+      const booking = await Booking.findById(bookingId);
+
+      // If payment is not made, release seats and delete booking
+      if (!booking.isPaid) {
+        const show = await Show.findById(booking.show);
+        booking.bookedSeats.forEach((seat) => {
+          delete show.occupiedSeats[seat];
+        });
+        show.markModified("occupiedSeats");
+        await show.save();
+        await Booking.findByIdAndDelete(booking._id);
+      }
+    });
+  }
+);
+
+// Inngest function to send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+  { id: "send-booking-confirmation-email" },
+  { event: "app/show.booked" },
+  async ({ event, step }) => {
+    const { bookingId } = event.data;
+
+    const booking = await Booking.findById(bookingId)
+      .populate({
+        path: "show",
+        populate: { path: "movie", model: "Movie" },
+      })
+      .populate("user");
+
+    await sendEmail({
+      to: booking.user.email,
+      subject: `Payment Confirmation: "${booking.show.movie.title}" booked !`,
+      body: `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+                   <h2>Hi ${booking.user.name},</h2>
+                   <p>Your booking for <strong style="color: #F84565;">"${
+                     booking.show.movie.title
+                   }"</strong> is confirmed.</p>
+                   <p>
+                      <strong>Date:</strong> ${new Date(
+                        booking.show.showDateTime
+                      ).toLocaleDateString("en-US", {
+                        timeZone: "Asia/Kolkata",
+                      })}<br/>
+                      <strong>Time:</strong> ${new Date(
+                        booking.show.showDateTime
+                      ).toLocaleTimeString("en-US", {
+                        timeZone: "Asia/Kolkata",
+                      })}
+                  </p>
+                  <p>Enjoy the show! 🍿</p>
+                  <p>Thanks for booking with us!<br/>- QuickShow Team</p>
+             </div>`,
+    });
+  }
+);
+
+// Inngest function to send notifications when a new show is Added
+const sendNewShowNotification = inngest.createFunction(
+  { id: "send-new-show-notification" },
+  { event: "app/show.added" },
+  async ({ event }) => {
+    const { movieTitle } = event.data;
+
+    const users = await User.find({});
+
+    for (const user of users) {
+      const userEmail = user.email;
+      const userName = user.name;
+
+      const subject = `🎞️ New Show Added: ${movieTitle}`;
+      const body = `<div>
+                      <h2>${userName}</h2>
+                    </div>`;
+
+      await sendEmail({
+        to: userEmail,
+        subject,
+        body,
+      });
+    }
+    return { message: "Notification sent." };
+  }
+);
+
 
 export const functions = 
 [syncUserCreation ,
  syncUserDeletion,
-syncUserUpdation];
+syncUserUpdation,
+releaseSeatsAndDeleteBooking];
